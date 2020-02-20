43e6265aa88835d4c68ff3dbfffc11fb00d816c7
Niraj Shah <niraj.shah@chicagotrading.com>
2020-02-20 10:19:10 -0600

    Added splunk server to deployment

--- network.tf
+++ network.tf
@@ -78,5 +78,17 @@ resource "azurerm_network_security_group" "abd-de-nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name = "Port8000"
+    priority = 1006
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "8000"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = var.abd-de-tags
 }
