3e7e147b5351cb10ca4e63aeef0353af52d0b9b0
Niraj Shah <niraj.shah@chicagotrading.com>
2020-02-25 16:03:23 -0600

    Added port 4646 for nomad UI

--- network.tf
+++ network.tf
@@ -90,5 +90,17 @@ resource "azurerm_network_security_group" "abd-de-nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name = "Port4646"
+    priority = 1007
+    direction = "Inbound"
+    access = "Allow"
+    protocol = "Tcp"
+    source_port_range = "*"
+    destination_port_range = "4646"
+    source_address_prefix = "*"
+    destination_address_prefix = "*"
+  }
+
   tags = var.abd-de-tags
 }
